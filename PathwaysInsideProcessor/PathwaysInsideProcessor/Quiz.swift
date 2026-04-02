95d857c4099d37df069f8c3cf00677d18f630e09
Hung Nguyen <165174644+HungNguyen922@users.noreply.github.com>
2026-04-02 11:59:06 -0500

    Update Quiz.swift

--- PathwaysInsideProcessor/PathwaysInsideProcessor/Quiz.swift
+++ PathwaysInsideProcessor/PathwaysInsideProcessor/Quiz.swift
@@ -1,5 +1,6 @@
 import foundation
 
+// represents mcq answer choices: the answer choice itself & the explanation for it
 struct MCQAnswerOption: Codable {
     let text: String
     let explanation: String
@@ -13,6 +14,8 @@ struct MCQ: Codable {
     let correctIdx: Int
 }
 
+// this takes in mcq.json to load in all the mcq for the quiz
+// i put the mcqs in the json for organization
 func loadMCQs(from filename: String) -> [MCQ] {
     do {
         let url = URL(fileURLWithPath: filename)
@@ -25,6 +28,7 @@ func loadMCQs(from filename: String) -> [MCQ] {
     }
 }
 
+// all the mcqs are loaded in to be used
 let allMCQs = loadMCQs(from: "mcqs.json")
 print("Loaded \(allMCQs.count) questions")
 
@@ -34,6 +38,7 @@ func checkMCQ(mcq: MCQ, selectedIdx: Int) -> Bool {
 }
 
 // DATAPATH PUZZLES are quite a bit more involved
+// represents processor components: ALU, Register File, Data Memory, Sign Extend, Control Unit
 struct Component: Codable {
     let id: String
     let name: String
@@ -41,11 +46,13 @@ struct Component: Codable {
     let outputs: [Port]
 }
 
+// represents inputs and output ports of components
 struct Port: Codable{
     let id: String
     let type: String
 }
 
+// represents the wires between components, also control signals
 struct Connection: Codable, Hashable {
     let fromComponent: String
     let fromPort: String
@@ -53,12 +60,13 @@ struct Connection: Codable, Hashable {
     let toPort: String
 }
 
+// represents puzzle questions where we provide components and the tester has to draw the wires
 struct Puzzle: Codable{
     let components: [Component]
     let correctConnections: [Connection]
 }
 
-
+// loads puzzle questions from json
 func loadPuzzles(from filename: String) -> [Puzzle] {
     do {
         let url = URL(fileURLWithPath: filename)
@@ -77,16 +85,18 @@ func checkPuzzle(userConnections: [Connection], puzzle: Puzzle) -> Bool {
     return Set(userConnections) == Set(puzzle.correctConnections)
 }
 
-// we have the quiz components, now we have to organize them to build an actual quiz
+// we have the quiz components, now we have to organize them to be options on the quiz
 enum QuizItem {
     case mcq(MCQ)
     case puzzle(Puzzle)
 }
 
+// this represents the actual quiz composed of quiz items
 struct Quiz {
     let items: [QuizItem]
 }
 
+// this builds the quiz to be used
 func generateQuiz(mcqs: [MCQ], puzzles: [Puzzle], totalItems: Int) -> Quiz {
     let mixedPool: [QuizItem] = 
         allMCQs.map { .mcq($0) } + 
@@ -97,3 +107,39 @@ func generateQuiz(mcqs: [MCQ], puzzles: [Puzzle], totalItems: Int) -> Quiz {
     return Quiz(items: selected)
 }
 
+// ObservableObject makes quizManager seeable across diff SwiftUIs; should be useful if we do multiple screens to display quiz questions
+// @Published makes the property automatically update related SwiftUI views
+class QuizManager: ObservableObject {
+    @Published var quiz: Quiz
+    @Published var currentIdx: Int = 0
+    @Published var score: Int = 0
+
+    init(quiz: Quiz) {
+        self.quiz = quiz
+    }
+
+    var currentItem: QuizItem? {
+        // guard is a control statement in swift that is meant to exit fast
+        guard currentIdx < quiz.items.count else { return nil }
+        return quiz.items[currentIdx]
+    }
+
+    func submitMCQAnswer(selectedIdx: Int) -> Bool {
+        // guard case is specifically for enums
+        guard case let .mcq(mcq) = currentItem else { return false }
+        let correct = checkMCQ(mcq: mcq, selectedIdx: selectedIdx)
+        if correct { score += 1 }
+        return correct
+    }
+
+    func submitPuzzleAnswer(userConnections: [Connection]) -> Bool {
+        guard case let .puzzle(puzzle) = currentItem else { return false }
+        let correct = checkPuzzle(userConnections: userConnections, puzzle: puzzle)
+        if correct { score += 1 }
+        return correct
+    }
+
+    func nextItem( {
+        currentIdx += 1
+    }
+}
